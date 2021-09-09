56a9658c81482cd5e02d0c9834541988c8e7d621
Vu Duc Anh <67143004+vuducanh0802@users.noreply.github.com>
2021-09-09 22:59:22 +0800

    change to grid default for simplicity

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import environment_2d
 import processing
 import numpy as np
 np.random.seed(4)
-env = environment_2d.Environment(10, 6, 2)
-processing.robo(env,random=True)
+env = environment_2d.Environment(10, 6, 5)
+processing.robo(env,random=False)
